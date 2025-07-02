a2eb868ab279b1d28e9f5a5803610d2e37143de9
jacob-hackney <jdhackney87@gmail.com>
2025-07-02 09:54:56 -0400

    New build(1.0.1)

--- build/src/KeyShortcut.js
+++ build/src/KeyShortcut.js
@@ -1,62 +1,62 @@
 export class KeyShortcut {
-    #keydowns = [];
-    #keyups = [];
-    #pressedKeys = {};
-    constructor(callback, ...keys) {
-        for (let key of keys) {
-            key = key.toLowerCase();
-            Object.defineProperty(this.#pressedKeys, key, {
-                value: false,
-                writable: true,
-            });
-            this.#keydowns.push((event) => {
-                event.preventDefault();
-                if (event.key.toLowerCase() === key) {
-                    this.#pressedKeys[key] = true;
-                    let allPressed = true;
-                    for (let k of keys) {
-                        if (!this.#pressedKeys[k]) {
-                            allPressed = false;
-                            break;
-                        }
-                    }
-                    if (allPressed) {
-                        callback(event);
-                    }
-                }
-            });
-            this.#keyups.push((event) => {
-                if (event.key.toLowerCase() === key) {
-                    this.#pressedKeys[key] = false;
-                }
-            });
+  #keydowns = [];
+  #keyups = [];
+  #pressedKeys = {};
+  constructor(callback, ...keys) {
+    for (let key of keys) {
+      key = key.toLowerCase();
+      Object.defineProperty(this.#pressedKeys, key, {
+        value: false,
+        writable: true,
+      });
+      this.#keydowns.push((event) => {
+        event.preventDefault();
+        if (event.key.toLowerCase() === key) {
+          this.#pressedKeys[key] = true;
+          let allPressed = true;
+          for (let k of keys) {
+            if (!this.#pressedKeys[k]) {
+              allPressed = false;
+              break;
+            }
+          }
+          if (allPressed) {
+            callback(event);
+          }
         }
+      });
+      this.#keyups.push((event) => {
+        if (event.key.toLowerCase() === key) {
+          this.#pressedKeys[key] = false;
+        }
+      });
     }
-    bound = false;
-    bind() {
-        if (this.bound) {
-            console.warn("[keylogic] Shortcut is already bound.");
-            return;
-        }
-        for (let listener of this.#keydowns) {
-            document.addEventListener("keydown", listener);
-        }
-        for (let listener of this.#keyups) {
-            document.addEventListener("keyup", listener);
-        }
-        this.bound = true;
+  }
+  bound = false;
+  bind() {
+    if (this.bound) {
+      console.warn("[keylogic] Shortcut is already bound.");
+      return;
     }
-    unbind() {
-        if (!this.bound) {
-            console.warn("[keylogic] Shortcut is already unbound.");
-            return;
-        }
-        for (let listener of this.#keydowns) {
-            document.removeEventListener("keydown", listener);
-        }
-        for (let listener of this.#keyups) {
-            document.removeEventListener("keyup", listener);
-        }
-        this.bound = false;
+    for (let listener of this.#keydowns) {
+      document.addEventListener("keydown", listener);
     }
+    for (let listener of this.#keyups) {
+      document.addEventListener("keyup", listener);
+    }
+    this.bound = true;
+  }
+  unbind() {
+    if (!this.bound) {
+      console.warn("[keylogic] Shortcut is already unbound.");
+      return;
+    }
+    for (let listener of this.#keydowns) {
+      document.removeEventListener("keydown", listener);
+    }
+    for (let listener of this.#keyups) {
+      document.removeEventListener("keyup", listener);
+    }
+    this.bound = false;
+  }
 }
