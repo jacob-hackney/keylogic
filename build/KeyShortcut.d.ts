fb2bb26a74eb55339b3ba38cb152820b9536a462
jacob-hackney <jdhackney87@gmail.com>
2025-07-02 09:54:56 -0400

    New build(1.0.1)

--- build/KeyShortcut.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class KeyShortcut {
-    #private;
-    constructor(callback: (event: KeyboardEvent) => void, ...keys: string[]);
-    bound: boolean;
-    bind(): void;
-    unbind(): void;
-}
